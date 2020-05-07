523b04d86d7eed25c78e54c93d36ff1e87429519
JohannesMackay <61296338+JohannesMackay@users.noreply.github.com>
2020-05-07 12:59:51 +1200

    Decied on fonts and colours, added a footer and setup hoow the home page is going to look. Made a Modal

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function toggleSidebar(){
   document.getElementById("sidebar").classList.toggle('active');
 }
+
+function togglePopup(){
+  document.getElementById("popup-1").classList.toggle("active");
+}
